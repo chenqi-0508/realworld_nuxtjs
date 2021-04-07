c97b958724d01b6b1d5653095a021d74f9f19dea
[username] <330320307@qq.com>
2021-04-07 15:55:28 +0800

    部署发布v0.1.5

--- pages/Layout/index.vue
+++ pages/Layout/index.vue
@@ -4,7 +4,7 @@
     <nav class="navbar navbar-light">
       <div class="container">
         <!-- <a class="navbar-brand" href="index.html">conduit</a> -->
-        <nuxt-link class="navbar-brand" to="/">new conduit</nuxt-link>
+        <nuxt-link class="navbar-brand" to="/">New Conduit</nuxt-link>
         <ul class="nav navbar-nav pull-xs-right">
           <li class="nav-item">
             <!-- Add "active" class when you're on that page" -->
